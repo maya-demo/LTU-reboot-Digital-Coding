6f8dd2095996fbcec04e31afe9ebcdae6d35680c
Maya <72470342+maya-demo@users.noreply.github.com>
2020-11-14 20:48:52 +0000

    Upload task 3.1.6

---
+++ scripts/task-3.1.6.js
@@ -0,0 +1,9 @@
+var priceOfBeer = 5;
+var priceOfPizza = 9.5;
+var pct = "3%";
+
+var totalBill = " Your total bill is"  (priceOfBeer + priceOfPizza)* pct;
+pct= parseFloat (pct) /100;
+
+console.log(totalBill);
+// total bill is $35.4- does not come up correctly 
