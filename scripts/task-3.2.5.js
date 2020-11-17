162055f05f33ac378fe57c7abfac9adcc814f0e7
Maya <72470342+maya-demo@users.noreply.github.com>
2020-11-17 21:40:50 +0000

    Complete task 3.2.5

---
+++ scripts/task-3.2.5.js
@@ -0,0 +1,16 @@
+
+var temperature= -5;
+
+if (temperature < 0) {
+    console.log('Stay inside');
+}
+else if (temperature < 30) {
+    console.log ('Wear a coat and hat');
+}
+
+else if (temperature < 50) {
+    console.log ('You should wear a coat');
+}
+ else {
+    console.log ('You are fine with just pants and vest');
+}
