acbf8e1ec5f0381b814338d2b6f9c5ba5c97ba01
Maya <72470342+maya-demo@users.noreply.github.com>
2020-11-17 22:14:28 +0000

    Complete 3.2.6

---
+++ scripts/task-3.2.6.js
@@ -0,0 +1,25 @@
+
+var temperature= -5;
+
+if (temperature < 0) {
+    console.log('Stay inside');
+}
+else if (temperature < 30) {
+    console.log ('Wear a coat and hat');
+}
+else if (temperature < 50) {
+    console.log ('You should wear a coat');
+}
+else if (temperature >50 && temperature === 55); {
+    console.log (' It is very hot');
+} 
+else if (temperature > 50 || temperature===55); {
+    console.log ('It is extremely hot, cover your skin');
+} 
+
+else {
+    console.log ('You are fine with just pants and vest');
+}
+
+//Logical operators && (AND)   || (OR)  ! (NOT)
+
