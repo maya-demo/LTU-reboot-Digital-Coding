8fc06268959f896edc8f7365a916e32bd2ac8d05
Maya <72470342+maya-demo@users.noreply.github.com>
2020-11-17 14:54:08 +0000

    Complete task 3.2.4

---
+++ scripts/task-3.2.4.js
@@ -0,0 +1,7 @@
+// if temp is below 50 you should put a coat on
+
+var temperature= 40;
+
+if (temperature < 50) {
+    console.log ('You should wear a coat');
+}
--- scripts/task3.2.3.js
+++
@@ -1,8 +0,0 @@
-function fullName(name, surname){
-    return name + ' ' + surname;
-}
-
-
-var userName= fullName ('Maya', 'Ivanov');
-
-console.log (userName);
