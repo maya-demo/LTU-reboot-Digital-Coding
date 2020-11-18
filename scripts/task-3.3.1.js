fc5a03b248067dfea40a95414afc35b629597741
Maya <72470342+maya-demo@users.noreply.github.com>
2020-11-18 13:59:41 +0000

    Task 3.3.1 complete

---
+++ scripts/task-3.3.1.js
@@ -0,0 +1,17 @@
+
+function percentageCalculator (number, percentage)
+
+{ 
+    var percantageOf= (number * percentage) /100;
+//BODMAS
+//could I use return? instead of var? 
+
+    return percantageOf;
+}
+
+console.log(percentageCalculator (300,25));
+
+var twentyFivePercentOf100 = percentageCalculator (100,25);
+
+    console.log(twentyFivePercentOf100);
+    
