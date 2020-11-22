55492757fc8fd1bdf0a55122c5d583f52542207d
Maya <72470342+maya-demo@users.noreply.github.com>
2020-11-22 21:45:34 +0000

    Complete task 3.4.3

---
+++ scripts/task-3.4.3.js
@@ -0,0 +1,10 @@
+var favouriteFoods = ['Pizza', 'Kebab', 'Salad'];
+
+for ( var index = 0; index < favouriteFoods.length; index++) {
+
+    console.log(favouriteFoods[index]);
+
+}
+console.log(favouriteFoods [0]); //pizza
+console.log(favouriteFoods [1]); // kebab
+console.log(favouriteFoods [2]); // salad
